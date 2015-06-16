e29c526dee3460e20677fb7635d1d44124c3f172
lukasthoms <luk@stho.ms>
2015-06-16 14:37:43 -0400

    Added custom prime selector

---
+++ PrimeTime/FISPrimeSelectorViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISPrimeSelectorViewController.h
+//  PrimeTime
+//
+//  Created by Lukas Thoms on 6/16/15.
+//  Copyright (c) 2015 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISPrimeSelectorViewController : UIViewController
+
+@end
